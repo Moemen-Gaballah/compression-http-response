b30048b61a1c5efa96a647748559caf4ff8b5e45
moemen gaballah <moemengaballa@gmail.com>
2023-07-14 19:10:40 +0300

    after compression

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const compression = require('compression');
 
 const app = express()
 
+app.use(compression({}))
+
 app.get('/', (req, res) => {
     const payload = "Faster app which uses less bandwidth too...";
-    res.send(payload.repeat(100000))
+    res.status(200).send(payload.repeat(100000))
 })
 
-app.listen(3000, () => console.log('server running on port 3000'));
+app.listen(3000, () => console.log('server running on port 3000 ...'));
